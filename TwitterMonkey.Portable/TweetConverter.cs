f1f4ec95fd1507ae6fd82c964881439f46f3521c
Niclas <niclas@mietz.io>
2015-04-04 15:39:50 +0200

    Add TweetConverter

---
+++ TwitterMonkey.Portable/TweetConverter.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace TwitterMonkey.Portable {
+  public class TweetConverter {
+    public static Tweet Convert(string json){
+      return JsonConvert.DeserializeObject<Tweet>(json);
+    }
+    public static List<Tweet> ConvertAll(string json){
+      return JsonConvert.DeserializeObject<List<Tweet>>(json);
+    }
+  }
+}
+
