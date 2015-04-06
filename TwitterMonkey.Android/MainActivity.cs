0c85088a35d15d53d9adc32bd79edf150d666b6a
Niclas <niclas@mietz.io>
2015-04-06 11:57:47 +0200

    MainActivity Cleanup

--- TwitterMonkey.Android/MainActivity.cs
+++ TwitterMonkey.Android/MainActivity.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Android.App;
 using Android.OS;
 using Android.Widget;
-using TwitterMonkey.Portable.Twitter;
 using TwitterMonkey.Portable;
 using Squareup.Picasso;
 using Android.Util;
@@ -15,25 +14,20 @@ using System.Collections.Generic;
 namespace TwitterMonkey {
   [Activity(Label = "TwitterMonkey", MainLauncher = true, Icon = "@drawable/icon")]
   public class MainActivity : Activity {
-    private const string TAG = "TwitterMonkey";
 
-    protected override async void OnCreate (Bundle bundle) {
+    private const string TAG = "TwitterMonkey";
+    protected override  void OnCreate (Bundle bundle) {
       base.OnCreate(bundle);
       // Set our view from the "main" layout resource
       SetContentView(Resource.Layout.Main);
-      var listView = FindViewById<ListView>(Resource.Id.listView1); 
-      // populate the listview with data
-      var jsonString    = await fetchJsonAsync(new Uri(Constants.JSON_URI));
-      var tweets        = TweetConverter.ConvertAll(jsonString);
-      listView.Adapter  = new TwitterAdapter(this, tweets);
+      init();
     }
 
-    private async Task<string> fetchJsonAsync (Uri uri) {
-      HttpWebRequest request = new HttpWebRequest(uri);
-      var resp = await request.GetResponseAsync();
-      StreamReader reader = new StreamReader (resp.GetResponseStream());
-      return await reader.ReadToEndAsync();
+    private async void init() {
+      var listView      = FindViewById<ListView>(Resource.Id.listView1); 
+      listView.Adapter  = new TwitterAdapter(this, await Tweets.Get());
     }
+
   }
 }
 
