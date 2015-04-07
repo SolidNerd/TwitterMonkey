0b271ed400f441c0f0475bbb51f10fe4a581b83e
Niclas <niclas@mietz.io>
2015-04-07 23:00:30 +0200

    Add Refresh Action Button

--- TwitterMonkey.Android/MainActivity.cs
+++ TwitterMonkey.Android/MainActivity.cs
@@ -10,25 +10,47 @@ using Android.Widget;
 using Squareup.Picasso;
 using TwitterMonkey.Portable.DataAccessObjects;
 using TwitterMonkey.Views;
+using Android.Support.V7.App;
+using Android.Support.V4.View;
+using Android.Views;
 
 
 namespace TwitterMonkey {
-  [Activity(Label = "TwitterMonkey", MainLauncher = true, Icon = "@drawable/icon")]
-  public class MainActivity : Activity {
+  [Activity(Label = "TwitterMonkey", MainLauncher = true, Icon = "@drawable/icon",Theme = "@style/Theme.AppCompat")]
+  public class MainActivity : ActionBarActivity {
 
     private const string TAG = "TwitterMonkey";
-    protected override  void OnCreate (Bundle bundle) {
+    private TwitterAdapter twitterAdapter;
+    public override bool OnCreateOptionsMenu (Android.Views.IMenu menu) {
+      // Inflate the menu items for use in the action bar
+
+      MenuInflater.Inflate(Resource.Menu.activity_actions, menu);
+      return base.OnCreateOptionsMenu(menu);
+    }
+
+    public override  bool OnOptionsItemSelected (Android.Views.IMenuItem item) {
+      updateTweets();
+      return true;
+    }
+
+    protected override void OnCreate (Bundle bundle) {
       base.OnCreate(bundle);
       // Set our view from the "main" layout resource
+      SupportRequestWindowFeature(WindowCompat.FeatureActionBar);
       SetContentView(Resource.Layout.Main);
       init();
     }
 
     private async void init() {
       var listView      = FindViewById<ListView>(Resource.Id.listView1); 
-      listView.Adapter  = new TwitterAdapter(this, await Tweets.Get());
+      listView.Adapter  = twitterAdapter = new TwitterAdapter(this, await Tweets.Get());
     }
 
+    private async void updateTweets(){
+      var tweets = await Tweets.Get();
+      Log.Debug(TAG, tweets.ToString());
+      twitterAdapter.Update(tweets);
+    }
   }
 }
 
