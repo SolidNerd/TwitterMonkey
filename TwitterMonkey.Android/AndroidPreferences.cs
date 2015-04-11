a6610b46abe102823d6c3285845d37d66117c590
Niclas <niclas@mietz.io>
2015-04-11 13:01:25 +0200

    AndroidPreferences Formatting

--- TwitterMonkey.Android/AndroidPreferences.cs
+++ TwitterMonkey.Android/AndroidPreferences.cs
@@ -13,12 +13,12 @@ namespace TwitterMonkey {
       SharedPreferences = sharedPreferences;
     }
 
-
     public override string GetString (string key, string defaultValue) {
       var preferenceString = SharedPreferences.GetString(key, defaultValue);
       Log.Debug("TwitterMonkey",string.Format("Load Setting String: {0} = {1}",key,preferenceString));
       return preferenceString;
     }
+
     public override bool SaveString (string key, string value) {
       var editor = SharedPreferences.Edit();
       editor.PutString(key, value);
